d9929bbc937a83613bc57937cd953b31b909f8b3
Eddie de Bear <eddie_debear@hotmail.com>
2019-10-09 16:09:33 +1100

    Cool theme tweeks to lighten text files and code

--- Themes/Cool.ps1
+++ Themes/Cool.ps1
@@ -6,9 +6,9 @@ $global:PSColorizer = @{
         CompressedDirectory = @{ Color = '#00f1f1'; Compressed =  $true; Directory = $true }
         Hidden     = @{ Color = '#505050'; Hidden = $true } 
         IgnoreFiles= @{ Color = '#909090'; Pattern = '^\.' }
-        Code       = @{ Color = '#0011ff'; Pattern = '\.(java|c|cpp|cs|js|css|html)$' }
-        Executable = @{ Color = '#11cc11'; Pattern = '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg|ps1|sh)$' }
-        Text       = @{ Color = '#006600'; Pattern = '\.(txt|cfg|conf|ini|csv|log|config|xml|yml|md|markdown)$' }
+        Code       = @{ Color = '#0077cc'; Pattern = '\.(java|c|cpp|cs|js|css|html)$' }
+        Executable = @{ Color = '#22ee22'; Pattern = '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg|ps1|sh)$' }
+        Text       = @{ Color = '#119911'; Pattern = '\.(txt|cfg|conf|ini|csv|log|config|xml|yml|md|markdown)$' }
         Compressed = @{ Color = '#006699'; Pattern = '\.(zip|tar|gz|rar|jar|war)$' }
         Directory  = @{ Color = '#00aadd'; Directory = $true}
         System     = @{ Color = '#00ffaa'; System = $true}
